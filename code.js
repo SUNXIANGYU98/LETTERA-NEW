97f1e0c028a8a15467ddd77b517c775f14759e78
SUN XIANGYU <s3477400485@gmail.com>
2025-04-01 13:32:03 +0200

    new foto

--- code.js
+++ code.js
@@ -43,25 +43,27 @@ export function disegnaPunto({
   gamma = 0,
 }) {
   stroke("black");
-
+  push();
   translate(x, y);
   rotate(frameCount);
-  image(img, 0, 0, 10, 100);
+  image(img, 0, 0, 100, 100);
+  pop();
 }
-
-/**
+let img;
+/**.
  * Carica le risorse necessarie
  * Esempio: carica immagini, suoni, ecc.
  */
-export function caricamentoRisorse() {}
+export function caricamentoRisorse() {
+  img = loadImage("./assets/Fault.png");
+}
 
 /**
  * Imposta le impostazioni iniziali
  * Esempio: impostazioni di frame rate, misura degli angoli, ecc.
  */
 export function impostazioni() {
-  img = loadImage(",/assets/Fault.PNG");
-  let img;
+  imageMode(CENTER);
 }
 
 /**
